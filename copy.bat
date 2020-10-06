f708b59cac7f390a9cca9b5626881a4f9e9458b1
Jack <jack@jackb.me>
2020-10-05 22:57:43 -0400

    removed a useless file

--- copy.bat
+++
@@ -1,2 +0,0 @@
-xcopy C:\Users\jb124652\Dropbox\Projects\Working\tm_driver\src\tm_driver C:\Users\jb124652\Dropbox\Omron\Ready\Source\omron_driver\omron_ral_driver\src\tm_driver
-xcopy C:\Users\jb124652\Dropbox\Projects\Working\tm_driver\include C:\Users\jb124652\Dropbox\Omron\Ready\Source\omron_driver\omron_ral_driver\include
